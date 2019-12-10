ce6b84e04e3c88c42b406bc9cd036856bd4e537b
developervirtuoso <developervirtuoso>
2019-12-10 21:12:05 +0530

    first commit

---
+++ src/main/resources/com/javatpoint/HelloController.java
@@ -0,0 +1,13 @@
+package com.javatpoint;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping(value="/")
+public class HelloController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView welcome() {
+		return new ModelAndView("NewFile","welcomeMessage","Hello Neeraj")
+	}
+}
